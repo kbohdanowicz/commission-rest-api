9a2040f452eec66b2e3874d3dd9b549a0bdbe361
Krzysztof Bohdanowicz <kbohdanowicz@sigma.ug.edu.pl>
2023-02-12 17:53:48 +0100

    Add app entry point

--- src/main/kotlin/kbohdanowicz/restapi/CommissionCalculatorApp.kt
+++ src/main/kotlin/kbohdanowicz/restapi/CommissionCalculatorApp.kt
@@ -1,5 +1,6 @@
 package kbohdanowicz.restapi
 
+import kbohdanowicz.restapi.cache.TransactionsCache
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,7 @@ import org.springframework.boot.runApplication
 class CommissionCalculatorApp
 
 fun main() {
+    // Object initialization
+    TransactionsCache
     runApplication<CommissionCalculatorApp>()
 }
